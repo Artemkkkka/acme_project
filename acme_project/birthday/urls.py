08be1f01ca829d763a3a3d8792177985d3d0ee79
Артём Брагин <bragin15bragin@yandex.ru>
2025-02-10 12:20:54 +0300

    Add custom error pages

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -7,10 +7,13 @@ app_name = 'birthday'
 urlpatterns = [
     # path('', views.birthday, name='create'),
     path('', views.BirthdayCreateView.as_view(), name='create'),
+    path('login_only/', views.simple_view),
     # path('list/', views.birthday_list, name='list'),
     path('list/', views.BirthdayListView.as_view(), name='list'),
     path('<int:pk>/', views.BirthdayDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
     # path('<int:pk>/delete/', views.delete_birthday, name='delete')
-    path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete')
+    path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'),
+    # path('<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path('<int:pk>/comment/', views.CongratulationCreateView.as_view(), name='add_comment'),
 ]
